a0e2d1fe6bb472f1e02c9766fc3b77f4eb9c9cc4
AlexGhiarasim <alexandru.ghiarasim03@gmail.com>
2024-04-26 23:04:38 +0300

    configure-server-Nodejs

--- InformatiX/app.js
+++ InformatiX/app.js
@@ -7,12 +7,12 @@ const server = http.createServer((req, res) => {
     let q = url.parse(req.url, true);
     let pathname = q.pathname;
 
-    let index = pathname.indexOf('?');
+    let index = pathname.indexOf('?');  // asta va trebui scos mai apoi dupa ce se face logarea !!
     if (index !== -1) {
         pathname = pathname.substring(0, index);
     }
     if (!pathname || pathname === '/') {
-        pathname = '/index.html'; 
+        pathname = '/index.html';
     }
 
     let filename;
@@ -21,24 +21,36 @@ const server = http.createServer((req, res) => {
     let contentType = 'text/html';
     switch (extname) {
         case '.html':
-            filename = './src/views' + pathname; 
+            filename = './src/views' + pathname;
             contentType = 'text/html';
             break;
         case '.css':
-            filename = './public/css' + pathname; 
-            contentType = 'text/css'; 
+            filename = '.' + pathname;
+            contentType = 'text/css';
+            break;
+        case '.jpg':
+        case '.jpeg':
+        case '.png':
+        case '.gif':
+            filename = '.' + pathname;
+            contentType = 'image/' + extname.substring(1); 
+            break;
+        case '.ttf':
+            filename = '.' + pathname;
+            contentType = 'font/ttf'; 
             break;
         default:
-            filename = './src/views' + pathname;
+            filename = '.' + pathname;
             break;
     }
 
-    fs.readFile(filename, function (err, data) {
+    fs.readFile(filename, (err, data) => {
         if (err) {
+            console.error("Wrong path: ", filename);
             res.writeHead(404, { 'Content-Type': 'text/html' });
             return res.end("Page not found");
         }
-        res.writeHead(200, { 'Content-Type': contentType });
+        res.writeHead(200, {'Content-Type': contentType });
         res.write(data);
         res.end();
     });
