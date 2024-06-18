f57c3aaf3b76175e0697cfdb7d088898f5da3cc0
Andra Ciobanu <ciobanuandramaria@gmail.com>
2024-06-18 17:59:23 +0300

    categories

---
+++ InformatiX/public/js/categories.js
@@ -0,0 +1,28 @@
+// categories.js
+const categories = {
+    clasa9: [
+        "Probleme elementare",
+        "Elemente ale limbajului",
+        "Tablouri unidimensionale",
+        "Tablouri bidimensionale",
+        "Probleme diverse"
+    ],
+    clasa10: [
+        "Subprograme",
+        "Recursivitate",
+        "Divide et Impera",
+        "Siruri de caractere",
+        "Structuri de date liniare",
+        "Liste alocate dinamic",
+        "Tipul struct"
+    ],
+    clasa11: [
+        "Teoria grafurilor",
+        "Programare dinamica",
+        "Metoda Greedy",
+        "Backtracking",
+        "Arbori"
+    ]
+};
+
+export default categories;
