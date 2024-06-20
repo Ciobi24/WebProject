2490d6ecd292e5e1671639651d2e34c6e1caabbe
Andra Ciobanu <ciobanuandramaria@gmail.com>
2024-06-20 17:10:56 +0300

    header responsive si reparat

--- InformatiX/src/models/db-config.js
+++ InformatiX/src/models/db-config.js
@@ -44,8 +44,8 @@ class Database {
 const connectionString = {
   host: "localhost",
   port: 3000,
-  user: "alex",
-  password: "alex123K!!",
+  user: "andra",
+  password: "andraciobanu24",
   database: "informatix"
 };
 
